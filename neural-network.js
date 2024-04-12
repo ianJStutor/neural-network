7c326453729bc711429217956a260f369df3e282
Ian Marshall <ian.js.tutor@gmail.com>
2024-04-12 12:08:04 -0400

    added lerp functionality

--- neural-network.js
+++ neural-network.js
@@ -17,7 +17,7 @@ export default class NeuralNetwork {
     static mutate(network, amount = 1) {
         network.levels.forEach((level) => {
             for (let i = 0; i < level.biases.length; i++) {
-                level.biases[i] = lerp(
+                level.biases[i] = NeuralNetwork.lerp(
                     level.biases[i],
                     Math.random() * 2 - 1,
                     amount
@@ -25,7 +25,7 @@ export default class NeuralNetwork {
             }
             for (let i = 0; i < level.weights.length; i++) {
                 for (let j = 0; j < level.weights[i].length; j++) {
-                    level.weights[i][j] = lerp(
+                    level.weights[i][j] = NeuralNetwork.lerp(
                         level.weights[i][j],
                         Math.random() * 2 - 1,
                         amount
@@ -33,6 +33,10 @@ export default class NeuralNetwork {
                 }
             }
         });
+    }
+
+    static lerp(a, b, t) {
+        return a + (b - a) * t;
     }
 }
 
